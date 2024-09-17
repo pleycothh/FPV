f46b6c6d5d45a528b5ece82f59f8fc7ee24c1e32
Zhuoheng Li <1050429760@qq.com>
2024-09-17 23:28:19 +1000

    add basic form

--- FPV.APP/src/app/sharing/models/productEntity.model.ts
+++ FPV.APP/src/app/sharing/models/productEntity.model.ts
@@ -5,6 +5,22 @@ export interface ProductEntity {
     // amount
     factor1: number;
     factor2: number;
+    factor3: number;
+    factor4: number;
+    factor5: number;
+    factor6: number;
+    factor7: number;
+    factor8: number;
+    factor9: number;
+    factor10: number;
+    factor11: number;
+    factor12: number;
+    factor13: number;
+    factor14: number;
+    factor15: number;
+    factor16: number;
+
+    
     sum: number;
 
     // time
